360ed3b7cc388eb08b647b7c2d4729f906df4629
NicoleGuzman <1633978@utp.edu.pe>
2023-07-03 17:14:24 -0500

    carpetas

---
+++ Petshop_gradle/lib/src/main/java/pe/edu/universidad/dto/DtoUsuarioLogin.java
@@ -0,0 +1,29 @@
+package pe.edu.universidad.dto;
+
+public class DtoUsuarioLogin {
+	private int id_usuario;
+	private String nombre_usuario;
+	private String contrasena;
+	
+	public int getId_usuario() {
+		return id_usuario;
+	}
+	public void setId_usuario(int id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+	
+	public String getNombre_usuario() {
+		return nombre_usuario;
+	}
+	public void setNombre_usuario(String nombre_usuario) {
+		this.nombre_usuario = nombre_usuario;
+	}
+	public String getContrasena() {
+		return contrasena;
+	}
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+	
+	
+}
---
+++ Petshop_gradle/lib/src/main/java/pe/edu/universidad/entity/Producto.java
@@ -0,0 +1,50 @@
+package pe.edu.universidad.entity;
+
+public class Producto {
+	private int id_producto;
+	private String nombre;
+	private String descripcion;
+	private double precio;
+	private int stock;
+	private String imagen;
+	
+	public int getId_producto() {
+		return id_producto;
+	}
+	public void setId_producto(int id_producto) {
+		this.id_producto = id_producto;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public String getImagen() {
+		return imagen;
+	}
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+	
+	
+	
+}

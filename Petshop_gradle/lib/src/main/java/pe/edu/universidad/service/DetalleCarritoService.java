63009b410ad1005d9c2ba5993228a4e5f53c03e1
NicoleGuzman <1633978@utp.edu.pe>
2023-07-26 00:26:36 -0500

    co

--- Petshop_gradle/lib/src/main/java/pe/edu/universidad/service/DetalleCarritoService.java
+++ Petshop_gradle/lib/src/main/java/pe/edu/universidad/service/DetalleCarritoService.java
@@ -1,10 +1,15 @@
 package pe.edu.universidad.service;
 
+import java.util.List;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.ws.rs.Path;
+
+import pe.edu.universidad.entidades.Usuario;
 
 @Path("DetalleCarritoService")
 @Stateless
@@ -12,5 +17,16 @@ import javax.ws.rs.Path;
 public class DetalleCarritoService {
 	@PersistenceContext(unitName = "petshop_jpa")
 	private EntityManager em;
+	
+	public Usuario buscarPorIdUsuario(int idUsuario) {
+        TypedQuery<Usuario> query = em.createQuery("SELECT u FROM Usuario u WHERE u.id_Usuario = :id_Usuario", Usuario.class);
+        query.setParameter("id_Usuario", idUsuario);
+        List<Usuario> resultados = query.getResultList();
+        if (resultados.isEmpty()) {
+            return null;
+        } else {
+            return resultados.get(0);
+        }
+    }
 
 }
